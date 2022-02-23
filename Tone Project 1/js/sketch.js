7dd61514b16fc98796a3afc0908ff2dba82fcd36
Grant-Candies <fredthewalrus@gmail.com>
2022-02-22 21:38:32 -0600

    Attempt to fix path

--- Tone Project 1/js/sketch.js
+++ Tone Project 1/js/sketch.js
@@ -1,8 +1,8 @@
 const sounds = new Tone.Players({
-  yuh : "Tone Project 1\media\YUH.mp3",
-  life : "Tone Project 1\media\life.mp3",
-  coin : "Tone Project 1\media\coin.mp3",
-  pause : "Tone Project 1\media\pause.mp3"
+  yuh : "YUH.mp3",
+  life : "life.mp3",
+  coin : "coin.mp3",
+  pause : "pause.mp3"
 })
 
 let button1;
